f4990febc9461169b02ae0f55fc7a43e468d6d63
novakvova <novakvova@gmail.com>
2022-06-20 20:52:46 +0300

    Begin working project

--- frontend/src/components/common/CropperDialog/index.tsx
+++ frontend/src/components/common/CropperDialog/index.tsx
@@ -1,28 +1,44 @@
 import classNames from "classnames";
-import React, { useState } from "react";
-import { boolean } from "yup";
+import React, { useState, useRef, LegacyRef, useEffect } from "react";
+import Cropper from "cropperjs";
 import "./style.css";
+import "cropperjs/dist/cropper.css";
 
 const CropperDialog: React.FC = () => {
+  const [show, setShow] = useState<boolean>(false);
 
-    const [show, setShow] = useState<boolean>(false);
-    const [image, setImage] = useState<string>("");
+  const [image, setImage] = useState<string>("");
+  const [cropperObj, setCropperObj] = useState<Cropper>();
+  const imgRef = useRef<HTMLImageElement>();
+  const imgPrev = useRef<HTMLImageElement>();
 
+
+  useEffect(() => {
+    if(imgRef.current)
+    {
+        const cropper = new Cropper(imgRef.current as HTMLImageElement, {
+            viewMode: 1,
+            aspectRatio: 1/1,
+            preview: imgPrev.current
+        });
+        setCropperObj(cropper);
+    }
+  }, []);
   const handleImageSelect = (e: React.FormEvent<HTMLInputElement>) => {
     let file = (e.currentTarget.files as FileList)[0];
     console.log("Select image cropper", file);
-    if(file){
-        const url=URL.createObjectURL(file);
-        console.log("url show image", url);
-        setShow(true);
-        setImage(url);
+    if (file) {
+      const url = URL.createObjectURL(file);
+      console.log("url show image", url);
+      setShow(true);
+      setImage(url);
+      cropperObj?.replace(url);
     }
     e.currentTarget.value = "";
   };
-  const toggleModal = () =>
-  {
-    setShow(prev=>!prev);
-  }
+  const toggleModal = () => {
+    setShow((prev) => !prev);
+  };
 
   return (
     <>
@@ -42,7 +58,7 @@ const CropperDialog: React.FC = () => {
       />
 
       <div className={classNames("modal fade show", { "custom-modal": show })}>
-        <div className="modal-dialog">
+        <div className="modal-dialog modal-lg">
           <div className="modal-content">
             <div className="modal-header">
               <h5 className="modal-title">Modal title</h5>
@@ -55,13 +71,34 @@ const CropperDialog: React.FC = () => {
               ></button>
             </div>
             <div className="modal-body">
-              <img
-                style={{ cursor: "pointer" }}
-                src={image}
-                width="450"
-                alt="Оберіть фото"
-              />
-              <p>Modal body text goes here.</p>
+              <div className="container">
+                <div className="row">
+                  <div className="col-md-8 col-lg-9">
+                    <div className="d-flex justify-content-center">
+                      <img
+                        style={{ cursor: "pointer" }}
+                        src={image}
+                        ref={imgRef as LegacyRef<HTMLImageElement>}
+                        width="100%"
+                        alt="Оберіть фото"
+                      />
+                    </div>
+                  </div>
+                  <div className="col-md-4 col-lg-3">
+                    <div className="d-flex justify-content-center">
+                      <div
+                        ref={imgPrev as LegacyRef<HTMLDivElement>}
+                        style={{
+                          height: "150px",
+                          width: "150px",
+                          border: "1px solid silver",
+                          overflow: "hidden",
+                        }}
+                      ></div>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
             <div className="modal-footer">
               <button
