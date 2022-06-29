f7b3c49a7fb79f4a41bd3acfa0b1d6398b8dbe8e
novakvova <novakvova@gmail.com>
2022-06-29 21:01:47 +0300

    Save image to server

--- MessageApi/MessageApi/Controllers/AccountController.cs
+++ MessageApi/MessageApi/Controllers/AccountController.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using Core.DTO.Account;
+using Core.Helpers;
 using Entities;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
@@ -13,16 +14,38 @@ namespace MessageApi.Controllers
     {
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly IMapper _mapper;
+        private readonly IWebHostEnvironment _env;
+        private readonly IConfiguration _configuration;
         public AccountController(UserManager<ApplicationUser> userManager,
-            IMapper mapper)
+            IMapper mapper, 
+            IWebHostEnvironment env,
+            IConfiguration configuration)
         {
             _userManager = userManager;
             _mapper = mapper;
+            _env = env;
+            _configuration = configuration;
         }
         [HttpPost]
         public IActionResult Register([FromBody]RegisterUserDTO register)
         {
             var user = _mapper.Map<ApplicationUser>(register);
+            try
+            {
+                    if (register.Photo != null)
+                    {
+                        string randomFilename = Path.GetRandomFileName() +
+                            ".jpeg";
+                        string pathSaveImages = InitStaticFiles
+                            .CreateImageByFileName(_env, _configuration,
+                                new string[] { "Folder" },
+                                randomFilename, register.Photo, false, false);
+                    }
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
             //доробить
             return Ok();
         }
